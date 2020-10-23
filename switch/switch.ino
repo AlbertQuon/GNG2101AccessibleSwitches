2f68f73423aee19819898d0c49563d4a232b72a9
victorchen01 <victor.chen1447@gmail.com>
2020-10-22 23:26:35 -0400

    Completed prototype

---
+++ switch/switch.ino
@@ -0,0 +1,43 @@
+#include "Mouse.h"
+
+// pins of the switch
+const int upButton = 10;
+const int downButton = 9;
+const int leftButton = 12;
+const int rightButton = 11;
+const int mouseButton = 8;
+
+int upState = digitalRead(upButton);
+int downState = digitalRead(downButton);
+int rightState = digitalRead(rightButton);
+int leftState = digitalRead(leftButton);
+int clickState = digitalRead(mouseButton);
+
+// variables will change:or reading the pushbutton status
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  pinMode(mouseButton, INPUT_PULLUP);
+}
+
+void loop() {
+
+  upState = digitalRead(upButton);
+  downState = digitalRead(downButton);
+  leftState = digitalRead(leftButton);
+  rightState = digitalRead(rightButton);
+  clickState = digitalRead(mouseButton);
+
+  if(upState == LOW) { Mouse.move(0, 1, 0); }
+  else if(downState == LOW) { Mouse.move(0, -1, 0); }
+  else if(leftState == LOW) { Mouse.move(-1, 0, 0); }
+  else if(rightState == LOW) { Mouse.move(1, 0, 0); }
+  else if(clickState == LOW) { Mouse.click(); }
+
+  delay(3);
+  
+}
