78740c3119e31a6bdc9a7a1a6f65a589846693ed
victorchen01 <victor.chen1447@gmail.com>
2020-12-06 22:37:05 -0500

    moved folder

---
+++ SwitchVersion2/SwitchVersion2.ino
@@ -0,0 +1,29 @@
+  
+#include "Mouse.h"
+
+// pins of the switch
+const int mouseButton = 8;
+
+int clickState = digitalRead(mouseButton);
+
+int d = 50;
+
+// variables will change:or reading the pushbutton status
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(mouseButton);
+}
+
+void loop() {
+
+  clickState = digitalRead(mouseButton);
+
+  if(clickState == LOW) { 
+    Serial.write("switch");
+    delay(100);
+  }
+
+  delay(d);
+  
+}
